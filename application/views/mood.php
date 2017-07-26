2496790e9b5d7c16da618ff2bbc2c7f31b2b31f5
Jack Owen <jack.bula@yahoo.com>
2017-07-26 11:59:52 +0800

    New

--- application/views/mood.php
+++ application/views/mood.php
@@ -1,13 +1,14 @@
 <div id="mood-container">
-	<div id="text">
+	<div class="mood-child">
 		<h1>How do you feel today?</h1>
 	</div>
-	<div id="moods">
-		<span class="emoji">😄</span>
-		<span class="emoji">😍</span>
-		<span class="emoji">😖</span>
-		<span class="emoji">😢</span>
-		<span class="emoji">😠</span>
-		<span class="emoji">😏</span>
+	<div class="mood-child">
+		<div class="emoji" id="Happy" data-toggle="tooltip" data-placement="top" title="Happy">😄</div>
+		<div class="emoji" id="Loved" data-toggle="tooltip" data-placement="top" title="Loved">😍</div>
+		<div class="emoji" id="Sick" data-toggle="tooltip" data-placement="top" title="Sick">😖</div>
+		<div class="emoji" id="Sad" data-toggle="tooltip" data-placement="top" title="Sad">😢</div>
+		<div class="emoji" id="Angry" data-toggle="tooltip" data-placement="top" title="Angry">😠</div>
+		<div class="emoji" id="Meh" data-toggle="tooltip" data-placement="top" title="Meh">😏</div>
+		<div class="emoji" id="Zoned" data-toggle="tooltip" data-placement="top" title="Zoned">😝</div>
 	</div>
 </div>
